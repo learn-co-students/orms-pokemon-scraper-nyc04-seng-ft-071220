ea00e8226bfc3a2665f15a1eb66b642680a29235
wilsonng21 <wilsongch92@gmail.com>
2020-08-12 12:43:49 -0400

    all deliverables completed

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,18 +2,22 @@ class Pokemon
 
     attr_accessor :id, :name, :type, :db
 
-    @@all = []
-
-    def initialize(attributes)
-        attributes.each {|key, value| self.send(("#{key}="), value)}
+    def initialize(id:, name:, type:, db:)
+        #attributes.each {|key, value| self.send(("#{key}="), value)}
+        @id = id
+        @name = name
+        @type = type
+        @db = db
     end
 
     def self.save(name, type, db)
-
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", [name, type])
     end
 
     def self.find(id, db)
-        
+        result = db.execute("SELECT * FROM pokemon WHERE id = ?", [id])
+        pokemon_info = result[0]
+        Pokemon.new(id: pokemon_info[0],name: pokemon_info[1],type: pokemon_info[2],db: db)
     end
 
 end
