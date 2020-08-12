8e9b25f02b7dff33b08922b807a27df309bb4f0b
Chenyun Zhang <morningchenyun@gmail.com>
2020-08-12 12:54:00 -0400

    done

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,19 +1,26 @@
+require 'pry'
 class Pokemon
 
     attr_reader :id, :name, :type, :db
 
     database_connection = SQLite3::Database.new('db/pokemon.db')
 
-    def initialize(db)
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @type =type
+        @name = name
         @db = db
-      
     end 
 
-   
+   def self.save(name,type,database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+   end
 
+   def self.find(id, database_connection)
+    new_pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?",id)
+    Pokemon.new(id:new_pokemon[0][0], name:new_pokemon[0][1], type:new_pokemon[0][2], db:database_connection)
+
+   end
 
 end
 
-# def self.save(name, breed, age, database_connection)
-#     database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
-#   end
