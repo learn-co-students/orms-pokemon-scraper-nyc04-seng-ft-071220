03f76048f42cbb3c749b94c5a0d92986a32c7706
fbado66 <fabdo66@gmail.com>
2020-08-12 11:45:49 -0400

    Setting first table in the db

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+
+    attr_reader :id, :name, :type, :db
+
+    database_connection = SQLite3::Database.new('db/pokemon.db')
+
+    def initialize(db)
+        @db = db
+      
+    end 
+
+   
+
+
 end
+
+# def self.save(name, breed, age, database_connection)
+#     database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+#   end
